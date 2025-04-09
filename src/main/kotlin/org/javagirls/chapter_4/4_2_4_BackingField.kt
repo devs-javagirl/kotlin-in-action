0d192ed6e4883919562b3defda07952e857d3f90
minamicr <minamicr@yahoo.com.br>
2025-04-09 20:29:55 -0300

    Exemplos birthYear

--- src/main/kotlin/org/javagirls/chapter_4/4_2_4_BackingField.kt
+++ src/main/kotlin/org/javagirls/chapter_4/4_2_4_BackingField.kt
@@ -24,9 +24,12 @@ class Person(var birthYear: Int) {
 fun main() {
     val user = UserBacking("Daniela")
     user.address = "Av Paulista, 900"
+    println(user.address)
 
     val person = Person(2040)
-    println("age in 2050 is ${person.ageIn2050}")
+    println("idade em 2050 para nascimento em 2040 é ${person.ageIn2050}")
     person.birthYear = 2045
-    println("age in 2050 is ${person.ageIn2050}")
+    println("idade em 2050 para nascimento em 2045 é ${person.ageIn2050}")
+    person.ageIn2050 = 20
+    println("considerando a idade de 20 anos em 2050, o ano de nascimento é ${person.birthYear}")
 }
