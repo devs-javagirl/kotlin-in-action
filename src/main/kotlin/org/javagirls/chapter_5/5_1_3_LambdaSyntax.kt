cafc36da511a59c6fd065af9609e4f8ce83e8edf
minamicr <minamicr@yahoo.com.br>
2025-05-07 20:31:58 -0300

    Chapter 5 - Comentários lambda

--- src/main/kotlin/org/javagirls/chapter_5/5_1_3_LambdaSyntax.kt
+++ src/main/kotlin/org/javagirls/chapter_5/5_1_3_LambdaSyntax.kt
@@ -1,5 +1,9 @@
 package org.javagirls.chapter_5
 
+// ESTRUTURA DE UMA LAMBDA
+// lambda sempre entre chaves
+// esquerda: parâmetros -> direita: corpo da funçção
+
 fun main() {
     { println(42) }()
 
@@ -15,11 +19,18 @@ fun main() {
     }
 
     val people = listOf(Person("Alice", 29), Person("Bob", 31))
+
+    // lambda (código entre chaves) sendo passada como argumento da função
     println(people.maxByOrNull({ p: Person -> p.age }))
+    // se a lambda for o último argumento da função, ela pode ser colocada fora dos parênteses
     println(people.maxByOrNull() { p: Person -> p.age })
+    // quando a lambda é o único argumento da função, você pode remover os parênteses vazios
     println(people.maxByOrNull { p: Person -> p.age })
+    // como está sendo utilizada uma coleção de Person, Kotlin infere o tipo, não é necessário declarar Person
     println(people.maxByOrNull { p -> p.age })
+    // it = nome implícito para o único parâmetro da lambda
     println(people.maxByOrNull { it.age })
+    // a lambda também pode delegar para uma função ou propriedade, podendo invocar um member reference
     println(people.maxByOrNull(Person::age))
 
     val getAge = { p: Person -> p.age }
--- src/main/kotlin/org/javagirls/chapter_5/5_1_4_VariablesScope.kt
+++ src/main/kotlin/org/javagirls/chapter_5/5_1_4_VariablesScope.kt
@@ -1,11 +1,13 @@
 package org.javagirls.chapter_5
 
+// lambda acessando parâmetros da função
 fun printMessagesWithPrefix(messages: Collection<String>, prefix: String) {
     messages.forEach {
         println("$prefix $it")
     }
 }
 
+// lambda alterando valores das variáveis
 fun printProblemCounts(responses: Collection<String>) {
     var clientErrors = 0
     var serverErrors = 0
