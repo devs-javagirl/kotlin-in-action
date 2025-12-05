3f041325e11a2f88c04f6593d2d1f23ed9f02229
minamicr <minamicr@yahoo.com.br>
2025-12-05 07:58:04 -0300

    chapter 11 - reified restrictions

---
+++ src/main/kotlin/org/javagirls/chapter_11/11_2_5_ReifiedRestrictions.kt
@@ -0,0 +1,36 @@
+package org.javagirls.chapter_11
+
+import java.util.*
+import kotlin.reflect.KClass
+
+inline fun <reified T> loadService() {
+    //não é possível criar novas instâncias da classe especificada como type parameter
+    //val x = T("Roberta", 34)
+    ServiceLoader.load(T::class.java)
+}
+
+data class Person (val name: kotlin.String) {
+    companion object {
+        fun fromString(str: kotlin.String): Person = Person(str)
+    }
+}
+
+fun main() {
+    // criar instância a partir de T em loadService
+    val person = Person("Sarah")
+    loadService<Person>()
+
+}
+
+/*
+//Now, you want a generic function that, given a class type and a string,
+calls the fromString method on that class's companion object:
+
+fun <T> createFromString(clazz: KClass<T>, str: String): T {
+    // This is pseudocode! You'd need reflection to actually call the companion object's method.
+    val companion = clazz.companionObjectInstance
+    val fromStringMethod = clazz.companionObject?.members?.first { it.name == "fromString" }
+    return fromStringMethod?.call(companion, str) as T
+}
+
+ */
