7b201088a218fa9e85db09ba4d4dcff3016c6f37
minamicr <minamicr@yahoo.com.br>
2025-12-11 20:46:41 -0300

    chapter 11 - mais exemplos de variance

--- src/main/kotlin/org/javagirls/chapter_11/11_3_1_Variance.kt
+++ src/main/kotlin/org/javagirls/chapter_11/11_3_1_Variance.kt
@@ -10,18 +10,33 @@ fun addAnswer(list: MutableList<Any>) {
 
 fun <T> addAnswerMutable(list: MutableList<T>, element: T) {
     list.add(element)
+    //list.add(12)
 }
 
 fun main() {
-    //printContents(listOf("abc", "bac"))
+    printContents(listOf("abc", "bac"))
+    printContents(listOf(1, 2, 3))
+    printContents(listOf(1, "abc", 5L))
 
-    val strings = mutableListOf("abc", "bac")
+    //Mix types in a collection, Kotlin will infer the type as MutableList<Any>
+    println("Teste de mutable list com vários tipos")
+    val test = mutableListOf("abc", 42, 1L)
+    println(test)
+
+    for (element in test) {
+        println("${element} is of type ${element::class.simpleName}")
+    }
+
     // no livro aponta erro ClassCastException: Integer cannot be cast to String
     // no compilador -> type mismatch required MutableList<any>
+    val strings = mutableListOf("abc", "bac")
     // addAnswer(strings)
 
     addAnswerMutable(strings, "efg")
+    // addAnswerMutable(strings, 12)
     println(strings)
 
     println(strings.maxBy { it. length })
+
+
 }
